8cdc44c0ff6533096918a0192114766ef0445b0c
unknown <winshelo@hotmail.com>
2023-02-22 12:50:51 -0500

    Light Program

--- app/src/main/java/com/winshelostlouis/flashcardapp/MainActivity.java
+++ app/src/main/java/com/winshelostlouis/flashcardapp/MainActivity.java
@@ -3,12 +3,42 @@ package com.winshelostlouis.flashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView question;
+    TextView reponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        question = findViewById(R.id.flascard_question_textview);
+        reponse = findViewById(R.id.flascard_reponse_textview);
+
+
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                question.setVisibility(View.INVISIBLE);
+                reponse.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                question.setVisibility(View.VISIBLE);
+                reponse.setVisibility(View.INVISIBLE);
+
+            }
+        });
     }
 }
