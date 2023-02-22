b528bc1d0549127abad7324df8be1c57a3c13255
unknown <winshelo@hotmail.com>
2023-02-22 14:45:50 -0500

    Last Change

--- app/src/main/java/com/winshelostlouis/flashcardapp/MainActivity.java
+++ app/src/main/java/com/winshelostlouis/flashcardapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.winshelostlouis.flashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView question;
-    TextView reponse;
+    CardView question;
+    CardView response;
+    Button button1, button2, button3, button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,27 +22,51 @@ public class MainActivity extends AppCompatActivity {
 
 
         question = findViewById(R.id.flascard_question_textview);
-        reponse = findViewById(R.id.flascard_reponse_textview);
+        response = findViewById(R.id.flascard_reponse_textview);
+        button1 = findViewById(R.id.reponse1);
+        button2 = findViewById(R.id.reponse2);
+        button3 = findViewById(R.id.reponse3);
+        button4 = findViewById(R.id.reponse4);
 
 
-        question.setOnClickListener(new View.OnClickListener() {
+              button1.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                     button1.setVisibility(View.INVISIBLE);
+                     Toast.makeText(MainActivity.this,"Mauvaise reponse", Toast.LENGTH_LONG).show();
+                   }
+              });
+
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    response.setVisibility(View.VISIBLE);
+                    question.setVisibility(View.INVISIBLE);
+                    button2.setVisibility(View.INVISIBLE);
+                    button4.setVisibility(View.VISIBLE);
+
+                }
+            });
+
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                question.setVisibility(View.INVISIBLE);
-                reponse.setVisibility(View.VISIBLE);
-
+                button3.setVisibility(View.INVISIBLE);
+                Toast.makeText(MainActivity.this,"Mauvaise reponse", Toast.LENGTH_LONG).show();
             }
         });
 
-        question.setOnClickListener(new View.OnClickListener() {
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                button4.setVisibility(View.INVISIBLE);
+                response.setVisibility(View.INVISIBLE);
                 question.setVisibility(View.VISIBLE);
-                reponse.setVisibility(View.INVISIBLE);
-
+                button1.setVisibility(View.VISIBLE);
+                button2.setVisibility(View.VISIBLE);
+                button3.setVisibility(View.VISIBLE);
             }
         });
+
     }
 }
