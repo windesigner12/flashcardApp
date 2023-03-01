19b7aca62ab291c784d5047e261e3f9a7c16704e
unknown <winshelo@hotmail.com>
2023-03-01 15:44:11 -0500

    Application Upgrade

--- app/src/main/java/com/winshelostlouis/flashcardapp/MainActivity.java
+++ app/src/main/java/com/winshelostlouis/flashcardapp/MainActivity.java
@@ -3,17 +3,30 @@ package com.winshelostlouis.flashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    CardView question;
-    CardView response;
-    Button button1, button2, button3, button4;
+    CardView questionCardView;
+    CardView responseCardView;
+
+    TextView questionTextView;
+    TextView responseTextView;
+    TextView totalQuestionTextView;
+    Button ansA, ansB, ansC;
+    Button submitBtn;
+
+
+    int score = 0;
+    int totalQuestion = QuestionsAnswers.question.length;
+    int currentQuestionIndex = 0;
+    String selectAnswer = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,52 +34,91 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        question = findViewById(R.id.flascard_question_textview);
-        response = findViewById(R.id.flascard_reponse_textview);
-        button1 = findViewById(R.id.reponse1);
-        button2 = findViewById(R.id.reponse2);
-        button3 = findViewById(R.id.reponse3);
-        button4 = findViewById(R.id.reponse4);
+        totalQuestionTextView = findViewById(R.id.totalquestions);
+
+        questionTextView = findViewById(R.id.flascard_question_textview);
+        responseTextView = findViewById(R.id.flascard_reponse_textview);
+
+        ansA = findViewById(R.id.reponse1);
+        ansB = findViewById(R.id.reponse2);
+        ansC = findViewById(R.id.reponse3);
+
+        submitBtn = findViewById(R.id.submitBtn);
 
 
-              button1.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View view) {
-                     button1.setVisibility(View.INVISIBLE);
-                     Toast.makeText(MainActivity.this,"Mauvaise reponse", Toast.LENGTH_LONG).show();
-                   }
-              });
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
 
-            button2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    response.setVisibility(View.VISIBLE);
-                    question.setVisibility(View.INVISIBLE);
-                    button2.setVisibility(View.INVISIBLE);
-                    button4.setVisibility(View.VISIBLE);
+        submitBtn.setOnClickListener(this);
 
-                }
-            });
+        totalQuestionTextView.setText("Total Question : "+ totalQuestion);
 
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                button3.setVisibility(View.INVISIBLE);
-                Toast.makeText(MainActivity.this,"Mauvaise reponse", Toast.LENGTH_LONG).show();
-            }
-        });
+        loadNewQuestion();
 
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                button4.setVisibility(View.INVISIBLE);
-                response.setVisibility(View.INVISIBLE);
-                question.setVisibility(View.VISIBLE);
-                button1.setVisibility(View.VISIBLE);
-                button2.setVisibility(View.VISIBLE);
-                button3.setVisibility(View.VISIBLE);
-            }
-        });
 
     }
+
+    @Override
+    public void onClick(View view) {
+        Button clickedButton = (Button) view;
+        ansA.setBackgroundColor(Color.GRAY);
+        ansB.setBackgroundColor(Color.GRAY);
+        ansC.setBackgroundColor(Color.GRAY);
+
+        if(clickedButton.getId() == R.id.submitBtn){
+
+            if(selectAnswer.equals(QuestionsAnswers.correctAswers[currentQuestionIndex]))
+            {
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+        }else {
+
+            selectAnswer = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.CYAN);
+        }
+
+    }
+
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion){
+            finishQuiz();
+            return;
+        }
+
+        questionTextView.setText(QuestionsAnswers.question[currentQuestionIndex]);
+        ansA.setText(QuestionsAnswers.choices[currentQuestionIndex][0]);
+        ansB.setText(QuestionsAnswers.choices[currentQuestionIndex][1]);
+        ansC.setText(QuestionsAnswers.choices[currentQuestionIndex][2]);
+    }
+
+    void finishQuiz(){
+
+        String passStatus = " ";
+        if(score > totalQuestion+0.60)
+        {
+            passStatus = "Passed";
+        }else {
+
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is " + score + " out of " + totalQuestion)
+                .setPositiveButton("Restart", (dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+    }
+
+    private void restartQuiz() {
+        score = 0;
+        currentQuestionIndex = 0;
+        loadNewQuestion();
+    }
+
 }
---
+++ app/src/main/java/com/winshelostlouis/flashcardapp/QuestionsAnswers.java
@@ -0,0 +1,25 @@
+package com.winshelostlouis.flashcardapp;
+
+public class QuestionsAnswers {
+
+    public static String question[] ={
+            "Which company owns the Android",
+            "Which one is not the programming language",
+            "Where you are watching this video"
+
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Samsung"},
+            {"Java","Kotlin","Notepad"},
+            {"Facebook","Whatsapp","Instagram"}
+
+    };
+
+    public static String correctAswers[] = {
+            "Google",
+            "Notepad",
+            "Facebook"
+    };
+
+}
